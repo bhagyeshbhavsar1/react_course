0ec3527e4ce56f7502ed61114d162eaff22496c9
bhagyeshbhavsar1 <bhagyeshbhavsar07@gamil.com>
2026-07-29 22:18:56 +0530

    notes_app project done

--- notes_app/src/App.jsx
+++ notes_app/src/App.jsx
@@ -4,11 +4,17 @@ import Background_img from "./components/Background_img";
 const App = () => {
   const [title, setTitle] = useState(" ");
   const [notes, setNotes] = useState(" ");
+  const [task, setTask] = useState([]);
 
   const eventHandler = (e) => {
     e.preventDefault();
-    console.log(title);
-    console.log(notes);
+
+    const copyTask = [...task];
+
+    copyTask.push({ title, notes });
+
+    setTask(copyTask);
+    console.log(copyTask);
 
     setTitle(" ");
     setNotes(" ");
@@ -24,16 +30,19 @@ const App = () => {
         }}
         className="relative z-10"
       >
-        <div className="item-center flex flex-col gap-10 p-11 w-1/2">
+        <div className="item-center flex flex-col gap-8 p-5 w-1/2">
+          <h1 className="text-4xl font-bold text-center pt-0 mb-0 text-black">
+            Notes App
+          </h1>
           <input
-            className="border border-black bg-transparent text-black px-3 py-2 rounded-full"
+            className="border border-red-500 bg-transparent text-black px-3 py-2 rounded-full"
             type="text"
             placeholder="Enter Notes Heading"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
           <input
-            className="border border-black bg-transparent text-black px-3 py-2 rounded-full"
+            className="border border-red-500 bg-transparent text-black px-3 py-2 rounded-full"
             type="text"
             placeholder="Write Notes Content"
             value={notes}
@@ -41,24 +50,26 @@ const App = () => {
               setNotes(e.target.value);
             }}
           />
-          <button className="bg-red-500 border border-white text-white px-4 py-2 rounded-full">
+          <button className="bg-red-500 border active:bg-red-300 border-white text-white px-4 py-2 rounded-full">
             Add Note
           </button>
         </div>
       </form>
 
       <div className="ml-10 mb-9">
-        <h2>Your Notes: </h2>
+        <h1 className="text-3xl">Your Notes: </h1>
         <br />
-        <div
-          id="right"
-          className="bg-gray-300 h-80 w-250 rounded-2xl p-4  overflow-x-auto"
-        >
+        <div id="right" className=" h-80 w-250 rounded-2xl p-4  overflow-auto">
           <div className="flex gap-4 w-max">
-            <div className="bg-red-500 h-70 w-80 rounded-2xl shrink-0"></div>
-            <div className="bg-red-500 h-70 w-80 rounded-2xl shrink-0"></div>
-            <div className="bg-red-500 h-70 w-80 rounded-2xl shrink-0"></div>
-            <div className="bg-red-500 h-70 w-80 rounded-2xl shrink-0"></div>
+            {task.map(function (elem) {
+              return (
+                <div className="bg-red-500 h-70 w-80 rounded-2xl shrink-0 p-5 text-white overflow-auto">
+                  <h3 className="text-4xl break-words">{elem.title}</h3>
+                  <br />
+                  <p className="leading-tight break-words">{elem.notes}</p>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
